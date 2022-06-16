4e9fd3db6cfd77cf093da47a527e3f661390d362
Abijan97 <abijanudara97@gmail.com>
2022-06-16 22:49:08 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Crypto_API.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: HomeController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: HomeController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: HomeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+      
+   
+
+     
+
+
+    
+    }
+}
